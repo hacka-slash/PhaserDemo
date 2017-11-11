f7a1769314de7fb8a00c3cfa536b92c3bd160f57
hacka-slash <placido.hoff@gmail.com>
2017-11-10 20:00:55 -0500

    Phaser Skeleton

---
+++ state1.js
@@ -0,0 +1,18 @@
+var demo = {};
+
+demo.state1 = function(){
+    
+};
+demo.state1.prototype = {
+    
+    preload: function(){
+        
+    },
+    create: function(){
+        
+    },
+    update: function(){
+        
+    }
+    
+};
